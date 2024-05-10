08765977cc6a320b2b165231c786b5d175c7aaee
stiliyan.dyankov <stiliyan.dyankov@trading212.com>
2024-05-10 09:02:48 +0300

    rm auth/redirect unused logic

--- extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-  console.log(message);
-});
-window.alert("yay!");
-console.log("Yaya");
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -21,51 +21,8 @@ chrome.runtime.onMessage.addListener(async ({ message }) => {
     });
   }
 
-  if (message === "Open new tab") {
-    chrome.tabs.create(
-      { url: "http://localhost:3000/login?loginFromExt=true" },
-      (tab) => {
-        createtTabId = tab.id;
-        chrome.scripting
-          .executeScript({ target: { tabId: tab.id }, func: injectedFunction })
-          .then((event) => {
-            // here to execute some logic - access to token
-            chrome.tabs.sendMessage(currentTabId, {
-              type: "USER_AUTHENTICATED",
-              payload: event,
-            });
-          });
-      },
-    );
-  }
-
   if (message === "REDIRECT_TO_CURR") {
     chrome.tabs.update(currentTabId, { active: true });
     chrome.tabs.remove(createtTabId);
   }
 });
-
-async function injectedFunction() {
-  const promise = new Promise((resolve) => {
-    window.postMessage({
-      message: "hey, its me, the injected script",
-      type: "FROM_CONTENT_SCRIPT",
-    });
-
-    window.addEventListener("successfulLogin", (event) => {
-      resolve(event);
-    });
-  });
-
-  window.addEventListener("message", (event) => {
-    if (event.data.type === "FROM_REACT_APP") {
-      setTimeout(() => {
-        chrome.runtime.sendMessage({ message: "REDIRECT_TO_CURR" });
-      }, 3000);
-    }
-  });
-
-  const event = await promise;
-
-  return event.detail.token;
-}
