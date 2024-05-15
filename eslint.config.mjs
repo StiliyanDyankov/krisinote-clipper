36f8a49f3fd8f391f729174d5f0cdc558ee459f3
stiliyan.dyankov <stiliyan.dyankov@trading212.com>
2024-05-15 08:33:18 +0300

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,13 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import eslintConfigPrettier from "eslint-config-prettier";
-
+import { unusedImports } from "eslint-plugin-unused-imports";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
-  eslintConfigPrettier
+  unusedImports,
+  eslintConfigPrettier,
 ];
